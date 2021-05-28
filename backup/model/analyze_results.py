bcc9b48e45429920fa0b7502fc5e50b6bb9b77b7
addisonpolcyn <addison@airtime.com>
2021-05-27 21:09:48 -0700

    pushed backup

---
+++ backup/model/analyze_results.py
@@ -0,0 +1,31 @@
+import json
+
+with open('model_results.json') as f:
+  data = json.load(f)
+
+# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
+# print(data)
+
+id = "4"
+sim = data[id]["user_similarity"]
+youtube = data[id]["duration_predictions"]
+
+def get_user_results(id):
+    sim = data[id]["user_similarity"]
+    youtube = data[id]["duration_predictions"]    
+
+    def get_top_n(dict, limit=10):
+        count = 0
+        print('-----------------')
+        for w in sorted(dict, key=dict.get, reverse=True):
+            count += 1
+            if count > limit:
+                break
+            print(w, dict[w])
+    print('|-----USER ' + id + '------|')
+    get_top_n(sim)
+    get_top_n(youtube)
+    print()
+
+get_user_results("4")
+get_user_results("295")
